c4ee1328d4c8cd2b80a2732f7d07f8b84adeea54
MCMrARM <mcmrarm@gmail.com>
2018-12-13 16:29:54 +0100

    Move making dmg-s to another script

--- __main__.py
+++ __main__.py
@@ -27,7 +27,6 @@ def display_stage(name):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--qt-path', help='Specify the Qt installation path', required=True)
-parser.add_argument('--dmg', help='Build a dmg', action='store_true')
 parser.add_argument('--force', help='Always remove the output directory', action='store_true')
 args = parser.parse_args()
 
@@ -122,8 +121,6 @@ QT_DEPLOY_OPTIONS = [path.join(QT_INSTALL_PATH, 'bin', 'macdeployqt'),  APP_OUTP
 QT_DEPLOY_OPTIONS.append('-qmldir=' + path.join(SOURCE_DIR, 'mcpelauncher-ui', 'mcpelauncher-ui-qt'))
 QT_DEPLOY_OPTIONS.append('-executable=' + path.abspath(path.join(APP_OUTPUT_DIR, 'Contents', 'MacOS', 'mcpelauncher-ui-qt')))
 QT_DEPLOY_OPTIONS.append('-executable=' + path.abspath(path.join(APP_OUTPUT_DIR, 'Contents', 'MacOS', 'msa-ui-qt')))
-if args.dmg:
-    QT_DEPLOY_OPTIONS.append('-dmg')
 call(QT_DEPLOY_OPTIONS)
 
 display_stage('App bundle has been built at {}!'.format(path.join(OUTPUT_DIR, APP_OUTPUT_NAME)))
---
+++ build_dmg.py
@@ -0,0 +1,78 @@
+from subprocess import check_call as call
+from os import path, makedirs, symlink, rmdir, remove, rename
+from shutil import copyfile, copytree
+from ds_store import DSStore
+from pprint import pprint
+from mac_alias import Alias
+import os
+
+SOURCE_DIR = 'source'
+OUTPUT_DIR = 'output'
+DMG_OUTPUT_NAME = 'Minecraft Bedrock Launcher.dmg'
+DMG_OUTPUT_PATH = path.join(OUTPUT_DIR,DMG_OUTPUT_NAME)
+DMG_MOUNT_PATH = '/Volumes/Minecraft Bedrock Launcher/'
+APP_OUTPUT_NAME = 'Minecraft Bedrock Launcher.app'
+APP_OUTPUT_DIR = path.join(OUTPUT_DIR, APP_OUTPUT_NAME)
+
+VOL_NAME = 'Minecraft Bedrock Launcher'
+
+if path.exists(DMG_OUTPUT_PATH):
+	remove(DMG_OUTPUT_PATH)
+
+# we assume here that sectors are 512B
+IMAGE_SECTOR_SIZE = 512
+def calc_size(p):
+	ret = 0
+	for entry in os.scandir(p):
+		ret += (entry.stat().st_size + IMAGE_SECTOR_SIZE - 1) // IMAGE_SECTOR_SIZE
+		ret += 16 + 4 * 8
+		if entry.is_dir():
+			ret += calc_size(entry.path)
+	return ret
+image_sectors = calc_size(APP_OUTPUT_DIR) + 1024 * 1024 * 4 // IMAGE_SECTOR_SIZE
+try:
+	makedirs('empty')
+	call(['hdiutil', 'create', '-fs', 'HFS+', '-format', 'UDRW', '-sectors', str(image_sectors), '-volname', VOL_NAME, '-srcfolder', 'empty', '-quiet', DMG_OUTPUT_PATH])
+finally:
+	rmdir('empty')
+call(['hdiutil', 'attach', '-noautoopen', '-mountpoint', DMG_MOUNT_PATH, '-quiet', DMG_OUTPUT_PATH])
+
+symlink("/Applications", path.join(DMG_MOUNT_PATH, "Applications"))
+copytree(APP_OUTPUT_DIR, path.join(DMG_MOUNT_PATH, APP_OUTPUT_NAME))
+copyfile(path.join(SOURCE_DIR, "dmg-background.png"), path.join(DMG_MOUNT_PATH, ".background.png"))
+
+with DSStore.open(path.join(DMG_MOUNT_PATH, '.DS_Store'), 'w+') as d:
+  d['Minecraft Bedrock Launcher.app']['Iloc'] = (152, 220-15)
+  d['Applications']['Iloc'] = (488, 220-10)
+  d['.']['bwsp'] = {
+  	'ShowStatusBar': False,
+  	'ShowPathbar': False,
+  	'ShowToolbar': False,
+  	'ShowTabView': False,
+  	'ContainerShowSidebar': False,
+  	'WindowBounds': '{{100, 350}, {640, 423}}',
+  	'ShowSidebar': False
+  }
+  d['.']['icvp'] = {
+  	'backgroundColorRed': 1.0,
+  	'backgroundColorGreen': 1.0,
+  	'backgroundColorBlue': 1.0,
+  	'iconSize': 64.0,
+  	'backgroundImageAlias': Alias.for_file(path.join(DMG_MOUNT_PATH, '.background.png')).to_bytes(),
+  	'textSize': 13.0,
+  	'backgroundType': 2,
+  	'gridOffsetX': 0.0,
+  	'gridOffsetY': 0.0,
+  	'showItemInfo': False,
+  	'viewOptionsVersion': 1,
+  	'arrangeBy': 'none',
+  	'labelOnBottom': True,
+  	'showIconPreview': True,
+  	'gridSpacing': 50.0
+  }
+  
+call(['hdiutil', 'detach', '-quiet', DMG_MOUNT_PATH])
+rename(DMG_OUTPUT_PATH, DMG_OUTPUT_PATH + '.tmp')
+call(['hdiutil', 'convert', '-format', 'UDZO', '-imagekey', 'zlib-level=6', '-o', DMG_OUTPUT_PATH, '-quiet', DMG_OUTPUT_PATH + '.tmp'])
+remove(DMG_OUTPUT_PATH + '.tmp')
+
