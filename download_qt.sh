955afec310bc7bb01613de60b4b044bc14d0b0c8
Christopher Homberger <christopher.homberger@web.de>
2020-05-09 23:47:53 +0200

    Sparkling Test

--- download_qt.sh
+++ download_qt.sh
@@ -47,3 +47,7 @@ install_module_extra qtwebengine qtwebengine
 echo "${COLOR_STATUS}Patching$2${COLOR_RESET}"
 cd qt/$QT_VERSION_SHORT/clang_64/
 ../../../patch_qt.sh
+
+curl -L -C - -o sparkle.tar.bz2 https://github.com/sparkle-project/Sparkle/releases/download/1.23.0/Sparkle-1.23.0.tar.bz2
+tar -xf ./sparkle.tar.bz2
+mv Sparkle.framework lib/
