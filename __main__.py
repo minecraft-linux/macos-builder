42c9a0cc4cf62d9ad6ded8189f8b0066d3cf5bd9
Christopher Homberger <christopher.homberger@web.de>
2020-05-22 21:45:40 +0200

    Nix

--- __main__.py
+++ __main__.py
@@ -70,7 +70,7 @@ def clone_repo(name, url, branch):
 
 display_stage("Downloading sources")
 clone_repo('msa', 'https://github.com/minecraft-linux/msa-manifest.git', 'master')
-clone_repo('mcpelauncher', 'https://github.com/minecraft-linux/mcpelauncher-manifest.git', 'ng')
+clone_repo('mcpelauncher', 'https://github.com/ChristopherHX/mcpelauncher-manifest.git', 'master')
 clone_repo('mcpelauncher-ui', 'https://github.com/minecraft-linux/mcpelauncher-ui-manifest.git', 'ng')
 
 # Build
