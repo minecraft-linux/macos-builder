e3e4b85f9f2f9bccdb5410229fb9a8f2f011fa87
ChristopherHX <christopher.homberger@web.de>
2020-09-05 00:24:32 +0200

    update to jnivm

--- __main__.py
+++ __main__.py
@@ -72,7 +72,7 @@ def clone_repo(name, url, branch):
 
 display_stage("Downloading sources")
 clone_repo('msa', 'https://github.com/minecraft-linux/msa-manifest.git', 'feature-xal')
-clone_repo('mcpelauncher', 'https://github.com/minecraft-linux/mcpelauncher-manifest.git', 'feature-xal')
+clone_repo('mcpelauncher', 'https://github.com/minecraft-linux/mcpelauncher-manifest.git', 'feature-jnivm')
 clone_repo('mcpelauncher-ui', 'https://github.com/minecraft-linux/mcpelauncher-ui-manifest.git', 'ng')
 if args.buildangle:
     clone_repo('osx-angle-ci', 'https://github.com/christopherhx/osx-angle-ci.git', 'master')
