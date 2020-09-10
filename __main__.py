fc5fcdb3b44b364b06ed8305ed59ecef1af569e5
ChristopherHX <christopher.homberger@web.de>
2020-09-10 19:18:05 +0200

    Add 1.16.100.55 (x86, x86_64)

--- __main__.py
+++ __main__.py
@@ -77,6 +77,7 @@ clone_repo('mcpelauncher-ui', 'https://github.com/minecraft-linux/mcpelauncher-u
 call(['git', 'checkout', 'ng-v0.0.1-rp1'], cwd=path.join(SOURCE_DIR, 'mcpelauncher-ui', 'mcpelauncher-ui-qt'))
 call(['git', 'checkout', 'feature-jnivm'], cwd=path.join(SOURCE_DIR, 'mcpelauncher', 'mcpelauncher-client'))
 call(['git', 'checkout', 'feature-1.16.20-32bit'], cwd=path.join(SOURCE_DIR, 'mcpelauncher', 'mcpelauncher-core'))
+call(['git', 'checkout', 'ng'], cwd=path.join(SOURCE_DIR, 'mcpelauncher', 'minecraft-imported-symbols'))
 if args.buildangle:
     clone_repo('osx-angle-ci', 'https://github.com/christopherhx/osx-angle-ci.git', 'master')
 
